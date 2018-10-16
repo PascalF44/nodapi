eccd4865a3c0e7b22e383edce0c02b76e16e9856
PascalF44 <pascal.fievre@ssx-solutions.fr>
2018-10-16 10:20:52 +0200

    appel sonde OK avec retour 200 et data ou retour 404

--- app.js
+++ app.js
@@ -1,25 +1,47 @@
-const http = require('http');
+const http = require("http");
+const https = require("https");
 
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3000;
 
-const server = http.createServer((req, res) => {
-    if (req.method === 'GET' && req.url === '/mesure') {
-        let heure = new Date();
-        let valeur = Math.random() * 100;
+const server = http
+  .createServer((req, res) => {
+    if (req.method === "GET" && req.url === "/mesure") {
+      const urlsonde =
+        "https://cloud.boltiot.com/remote/e08179db-97d0-49fb-82cc-d7f4f42222f1/analogRead?pin=A0&deviceName=BOLT5034218";
+      let heure = new Date();
+      let valeur = "";
 
-        let body = {
-            "timestamp": heure,
-            "value": valeur
-        };
-        res.writeHead(200, {
-            "Content-Type": "application/json"
+      https.get(urlsonde, rep => {
+        rep.setEncoding("utf8");
+        let body = "";
+
+        rep.on("data", data => {
+          body += data;
         });
-        res.end(JSON.stringify(body));
+
+        rep.on("end", () => {
+          body = JSON.parse(body);
+          console.log("success: " + body.success);
+          console.log("value: " + body.value);
+          valeur = body.value / 10;
+          body = {
+            timestamp: heure,
+            value: valeur
+          };
+
+          res.writeHead(200, {
+            "Content-Type": "application/json"
+          });
+
+          res.end(JSON.stringify(body));
+        });
+      });
     } else {
-        res.statusCode = 404;
-        res.end();
+      res.statusCode = 404;
+      res.end();
     }
-}).listen(port, hostname, () => {
+  })
+  .listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
-});
+  });
